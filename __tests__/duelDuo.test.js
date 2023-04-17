e7c1d01566c4b9e46c7447a2666591c8b2652603
Morgan Herrmann <morganherrmann4@gmail.com>
2023-04-17 00:21:05 -0700

    updated

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -16,3 +16,25 @@ describe("Duel Duo tests", () => {
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
 });
+
+describe("game functionality", () => {
+  test("clicking the Draw button displays the div with id = 'choices'", () => {
+    const drawButton = document.querySelector("#draw-button");
+    const choicesDiv = document.querySelector("#choices");
+    expect(choicesDiv.classList.contains("hidden")).toBe(true);
+    drawButton.click();
+    expect(choicesDiv.classList.contains("hidden")).toBe(false);
+  });
+
+  test("when a bot is 'Removed from Duo', it goes back to 'choices'", () => {
+    const botToRemove = document.querySelector("#bot-1");
+    const addBotButton = document.querySelector("#add-bot-1");
+    const removeBotButton = document.querySelector("#remove-bot-1");
+    const choicesDiv = document.querySelector("#choices");
+    const playerDuoDiv = document.querySelector("#player-duo");
+    addBotButton.click();
+    expect(playerDuoDiv.contains(botToRemove)).toBe(true);
+    removeBotButton.click();
+    expect(choicesDiv.contains(botToRemove)).toBe(true);
+  });
+});
--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,15 @@
 const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
-  // CODE HERE
-});
+    test("returns an array", () => {
+      const arr = [1, 2, 3, 4, 5];
+      const result = shuffle(arr);
+      expect(Array.isArray(result)).toBe(true);
+    });
+  
+    test("returns an array of the same length as the argument", () => {
+      const arr = [1, 2, 3, 4, 5];
+      const result = shuffle(arr);
+      expect(result.length).toBe(arr.length);
+    });
+  });
